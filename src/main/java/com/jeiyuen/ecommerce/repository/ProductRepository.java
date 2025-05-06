df42211152d8976d81f8b3819c0172e14c7f31b5
gjuanillo <gjuanillo.dev@gmail.com>
2025-05-06 14:55:14 +0800

    Setup Product Service & Controller

--- src/main/java/com/jeiyuen/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/jeiyuen/ecommerce/repository/ProductRepository.java
@@ -1,5 +1,11 @@
 package com.jeiyuen.ecommerce.repository;
 
-public interface ProductRepository{
+import com.jeiyuen.ecommerce.model.Product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long>{
 
 }
---
+++ src/main/java/com/jeiyuen/ecommerce/service/ProductService.java
@@ -0,0 +1,5 @@
+package com.jeiyuen.ecommerce.service;
+
+public interface ProductService{
+
+}
