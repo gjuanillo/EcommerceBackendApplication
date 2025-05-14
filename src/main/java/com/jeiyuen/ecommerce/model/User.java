bbd57c0dbf58370f05a4d61f677c786cb48a1bbd
gjuanillo <gjuanillo.dev@gmail.com>
2025-05-14 14:41:27 +0800

    Establish User to Role/Product relationship

--- src/main/java/com/jeiyuen/ecommerce/model/User.java
+++ src/main/java/com/jeiyuen/ecommerce/model/User.java
@@ -1,26 +1,41 @@
 package com.jeiyuen.ecommerce.model;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 @Entity
-@Table(name = "users")
-public class User{
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "username"),
+        @UniqueConstraint(columnNames = "email")
+})
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long userId;
 
     @NotBlank
     @Size(max = 20)
+    @Column(name = "username")
     private String userName;
 
     @NotBlank
@@ -32,11 +47,17 @@ public class User{
     @Size(min = 12, max = 60)
     private String password;
 
+    @ManyToMany(cascade = { CascadeType.PERSIST, CascadeType.MERGE }, fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    @OneToMany(mappedBy = "user", cascade = { CascadeType.PERSIST, CascadeType.MERGE }, orphanRemoval = true)
+    private Set<Product> products;
+
     public User() {
     }
 
-    public User(@NotBlank @Size(max = 20) String userName, @NotBlank @Size(max = 50) @Email String email,
-            @NotBlank @Size(min = 12, max = 60) String password) {
+    public User(String userName, String email, String password) {
         this.userName = userName;
         this.email = email;
         this.password = password;
@@ -74,9 +95,17 @@ public class User{
         this.password = password;
     }
 
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
     @Override
     public int hashCode() {
-        return Objects.hash(userId, userName, email, password);
+        return Objects.hash(userId, userName, email, password, roles);
     }
 
     @Override
@@ -92,7 +121,8 @@ public class User{
         }
         User other = (User) obj;
         return Objects.equals(userId, other.userId) && Objects.equals(userName, other.userName)
-                && Objects.equals(email, other.email) && Objects.equals(password, other.password);
+                && Objects.equals(email, other.email) && Objects.equals(password, other.password)
+                && Objects.equals(roles, other.roles);
     }
 
 }
