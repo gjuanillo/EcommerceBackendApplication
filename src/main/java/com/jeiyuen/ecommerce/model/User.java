e776760d482e58dfabcda79f8ca16ecbfdef8cee
gjuanillo <gjuanillo.dev@gmail.com>
2025-05-14 12:25:03 +0800

    Create User Entity Model

---
+++ src/main/java/com/jeiyuen/ecommerce/model/User.java
@@ -0,0 +1,98 @@
+package com.jeiyuen.ecommerce.model;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "users")
+public class User{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @NotBlank
+    @Size(max = 20)
+    private String userName;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 12, max = 60)
+    private String password;
+
+    public User() {
+    }
+
+    public User(@NotBlank @Size(max = 20) String userName, @NotBlank @Size(max = 50) @Email String email,
+            @NotBlank @Size(min = 12, max = 60) String password) {
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, userName, email, password);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        User other = (User) obj;
+        return Objects.equals(userId, other.userId) && Objects.equals(userName, other.userName)
+                && Objects.equals(email, other.email) && Objects.equals(password, other.password);
+    }
+
+}
