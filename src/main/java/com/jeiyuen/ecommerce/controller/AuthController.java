c74c7d1639493976f492fecf4a89d6d0d52b4300
gjuanillo <gjuanillo.dev@gmail.com>
2025-05-16 09:06:38 +0800

    Set User Auth to Cookie Based

--- src/main/java/com/jeiyuen/ecommerce/controller/AuthController.java
+++ src/main/java/com/jeiyuen/ecommerce/controller/AuthController.java
@@ -22,7 +22,9 @@ import com.jeiyuen.ecommerce.security.response.UserInfoResponse;
 import com.jeiyuen.ecommerce.security.services.UserDetailsImpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -69,12 +71,13 @@ public class AuthController {
         }
         SecurityContextHolder.getContext().setAuthentication(authentication);
         UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-        String jwtToken = jwtUtils.generateTokenfromUsername(userDetails);
+        // Changed from generated token into generated cookie
+        ResponseCookie jwtCookie = jwtUtils.generateJwtCookie(userDetails);
         List<String> roles = userDetails.getAuthorities().stream().map(item -> item.getAuthority())
                 .collect(Collectors.toList());
-        UserInfoResponse response = new UserInfoResponse(userDetails.getId(), jwtToken, userDetails.getUsername(),
+        UserInfoResponse response = new UserInfoResponse(userDetails.getId(), userDetails.getUsername(),
                 roles);
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, jwtCookie.toString()).body(response);
     }
 
     @PostMapping("/signup")
